1d34b0c3ba6eaa94f11635488c8e27686a5a2d72
saltyjoss <joss.salton@gmail.com>
2025-10-15 13:05:00 +0100

    refactor(ODESolverTests): Refractored test header in ODESolverTests + Renaming to ODETests

---
+++ ComputationalMathematics/ODETests.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <functional>
+
+namespace NumericalMethodsTests
+{
+
+    namespace NumericalMethodTests {
+
+        class ODESolverTests {
+        public:
+            static double EulerMethodTest(std::function<double(double, double)> f);
+            static double RungeKutta4MethodTest(std::function<double(double, double)> f);
+            static double FiniteDifferenceMethodTest(std::function<double(double, double)> f);
+        };
+
+        void ODETests(); // wrapper function to run all tests
+    }
+}
