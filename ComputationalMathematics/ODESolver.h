9b68a1449b4972d388464c0b976ef5aaed3ee6eb
saltyjoss <joss.salton@gmail.com>
2025-10-15 12:08:55 +0100

    chore(ODESolver Header): Changed all double t0 to double x0 value for personal clarity due to future plans
    - does not hurt logic
    - However future plan with this project may lead me to determining variables so for now I want to use x and y when possible
    - PURELY PERSONAL CHANGE

--- ComputationalMathematics/ODESolver.h
+++ ComputationalMathematics/ODESolver.h
@@ -5,8 +5,8 @@ namespace NumericalMethods
 {
 	namespace ODESolver 
 	{
-		double EulerMethod(double y0, double t0, int steps, double h, std::function<double(double, double)> f);
-		double RungeKutta4Method(double y0, double t0, int steps, double h, std::function<double(double, double)> f);
-		double FiniteDifferenceMethod(double y0, double t0, int steps, double h, std::function<double(double, double)> f);
+		double EulerMethod(double y0, double x0, int steps, double h, std::function<double(double, double)> f);
+		double RungeKutta4Method(double y0, double x0, int steps, double h, std::function<double(double, double)> f);
+		double FiniteDifferenceMethod(double y0, double x0, int steps, double h, std::function<double(double, double)> f);
 	}
 }
