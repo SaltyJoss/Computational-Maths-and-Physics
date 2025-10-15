ed61174e22398918b63761f8e48e03ebc8b65396
saltyjoss <joss.salton@gmail.com>
2025-10-15 20:36:45 +0100

    test(results): Add 4 variation of the same tests of each method
    - tests the output for different h
    - provides insight into the error per step (Documented using big(O) in personal notes for conciseness)

--- ComputationalMathematics/Main.cpp
+++ ComputationalMathematics/Main.cpp
@@ -8,7 +8,9 @@ void ODESolver(std::function<double(double, double)> f);
 int main() {
     auto f = [](double t, double y) { return t + y; };
 
-	std::cout << "Testing ODE Solvers with dy/dt = t + y, y(0) = 0:\n\n";
+	std::cout 
+		<< "TESTING ODE SOLVERS:\n" 
+		<< "=====\n\n";
 	ODESolver(f);
 
 	return 0;
@@ -16,20 +18,33 @@ int main() {
 
 // Function to test ODE solvers
 void ODESolver(std::function<double(double, double)> f) {
-	double eulerResult = NumericalMethods::ODESolver::EulerMethod(1.0, 0.0, 1, 0.25, f);
-	double fdResult = NumericalMethods::ODESolver::FiniteDifferenceMethod(1.0, 0.0, 1, 0.25, f);
-	double rk4Result = NumericalMethods::ODESolver::RungeKutta4Method(1.0, 0.0, 1, 0.25, f);
+	double eulerResult = NumericalMethods::ODESolver::EulerMethod(1.0, 0.0, 1, 0.1, f);
+	double midpointResult = NumericalMethods::ODESolver::MidpointMethod(1.0, 0.0, 1, 0.1, f);
+	double rk4Result = NumericalMethods::ODESolver::RungeKutta4Method(1.0, 0.0, 1, 0.1, f);
+
+	double eulerResult2 = NumericalMethods::ODESolver::EulerMethod(1.0, 0.0, 1, 0.5, f);
+	double midpointResult2 = NumericalMethods::ODESolver::MidpointMethod(1.0, 0.0, 1, 0.5, f);
+	double rk4Result2 = NumericalMethods::ODESolver::RungeKutta4Method(1.0, 0.0, 1, 0.5, f);
+
+	double eulerResult3 = NumericalMethods::ODESolver::EulerMethod(1.0, 0.0, 1, 0.25, f);
+	double midpointResult3 = NumericalMethods::ODESolver::MidpointMethod(1.0, 0.0, 1, 0.25, f);
+	double rk4Result3 = NumericalMethods::ODESolver::RungeKutta4Method(1.0, 0.0, 1, 0.25, f);
+
+	double eulerResult4 = NumericalMethods::ODESolver::EulerMethod(1.0, 0.0, 1, 0.01, f);
+	double midpointResult4 = NumericalMethods::ODESolver::MidpointMethod(1.0, 0.0, 1, 0.01, f);
+	double rk4Result4 = NumericalMethods::ODESolver::RungeKutta4Method(1.0, 0.0, 1, 0.01, f);
 
 	std::cout
 		<< "Running EulerMethod Test: " << "\n"
 		<< "-----\n"
-		<< "\tOutput: " << eulerResult << "\n\n\n"
-		<< "Running FiniteDifferenceMethod Test: " << "\n"
+		<< "\tOutput: " << eulerResult << ", " << eulerResult2 << ", " << eulerResult3 << ", " << eulerResult4 << "\n\n\n"
+		<< "Running MidpointMethod Test: " << "\n"
 		<< "-----\n"
-		<< "\tOutput: " << fdResult << "\n\n\n"
+		<< "\tOutput: " << midpointResult << ", " << midpointResult2 << ", " << midpointResult3 << ", " << midpointResult4 << "\n\n\n"
 		<< "Running Runge-Kutta4Method Test: " << "\n"
 		<< "-----\n"
-		<< "\tOutput: " << rk4Result << "\n\n\n";
+		<< "\tOutput: " << rk4Result << ", " << rk4Result2 << ", " << rk4Result3 << ", " << rk4Result4 << "\n\n\n";
+
 }
 /* Predicted Output (through my own calculations):
  * eulerResult -> 
