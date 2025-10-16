043c24a5c8bcf25715be94a5b903b6ea723b511c
saltyjoss <joss.salton@gmail.com>
2025-10-16 10:57:57 +0100

    Chore: Add project-local vcpkg and GoogleTest setup
    - Added vcpkg folder with bootstrapped setup for dependency management
    - Installed GoogleTest via vcpkg for unit testing
    - Configured CMake to build MathLib and link tests against GoogleTest

---
+++ MathLib/build/tests/test_ExplicitSolvers[1]_tests.cmake
@@ -0,0 +1,1 @@
+set(  test_ExplicitSolvers_TESTS)
--- MathLib/tests/test_ExplicitSolvers.cpp
+++ MathLib/tests/test_ExplicitSolvers.cpp
@@ -1,4 +1,6 @@
 #include "mathlib/ODE/ExplicitSolvers.hpp"
+
+#include <gtest/gtest.h>
 #include <iostream>
 #include <cmath>
 #include <functional>
@@ -21,21 +23,21 @@ int test(std::function<double(double, double)> f) {
 
 void testEulerMethod(std::function<double(double, double)> f) {
 	double y = mathlib::ODE::EulerMethod(1, 0, 1, 0.2, f);
-	std::cout << "Testing Euler Method:\n---\n\n" << "Approx: " << y
+	std::cout << "Testing Euler Method:\n---\n" << "\tApprox: " << y
 		<< ", Exact: " << std::exp(1.0)
-		<< ", Error: " << std::abs(y - std::exp(1.0)) << "\n\n\n";
+		<< ", Error: " << std::abs(y - std::exp(1.0)) << "\n\n";
 }
 
 void testRK2Method(std::function<double(double, double)> f) {
 	double y = mathlib::ODE::RK2Method(1, 0, 1, 0.2, f);
-	std::cout << "Testing RK2 Method:\n---\n\n"
-		<< "Approx: " << y << ", Exact: " << std::exp(1.0)
-		<< ", Error: " << std::abs(y - std::exp(1.0)) << "\n\n\n";
+	std::cout << "Testing RK2 Method:\n---\n" << "\tApprox: " << y 
+		<< ", Exact: " << std::exp(1.0)
+		<< ", Error: " << std::abs(y - std::exp(1.0)) << "\n\n";
 }
 
 void testRK4Method(std::function<double(double, double)> f) {
 	double y = mathlib::ODE::RK4Method(1, 0, 1, 0.2, f);
-	std::cout << "Testing RK4 Method:\n---\n\n" << "Approx: "
-		<< y << ", Exact: " << std::exp(1.0)
-		<< ", Error: " << std::abs(y - std::exp(1.0)) << "\n\n\n";
+	std::cout << "Testing RK4 Method:\n---\n" << "\tApprox: " << y 
+		<< ", Exact: " << std::exp(1.0)
+		<< ", Error: " << std::abs(y - std::exp(1.0)) << "\n\n";
 }
