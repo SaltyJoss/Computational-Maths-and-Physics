6c057688ac2cbc9f45dd942f5c9e5d125c4e16a9
saltyjoss <joss.salton@gmail.com>
2025-10-17 20:09:18 +0100

    feat(tests): Add main to test_main.cpp
    - allows all tests to run
    - more dynamic process

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,6 +3,8 @@
  */
 #include <gtest/gtest.h>
 
-TEST(PlaceholderTest, DoesNothing) {
-	ASSERT_TRUE(true);
+#include <gtest/gtest.h>
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
