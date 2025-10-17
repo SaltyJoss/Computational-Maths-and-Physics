ececcd17767141fd8512d96307f6af0f651dbdba
saltyjoss <joss.salton@gmail.com>
2025-10-17 20:04:13 +0100

    refactor(cmake, source files)

---
+++ out/build/x64-Debug/tests/UnitTests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Users/Joss/source/repos/PxM/out/build/x64-Debug/tests/UnitTests[1]_tests.cmake")
+  include("C:/Users/Joss/source/repos/PxM/out/build/x64-Debug/tests/UnitTests[1]_tests.cmake")
+else()
+  add_test(UnitTests_NOT_BUILT UnitTests_NOT_BUILT)
+endif()
