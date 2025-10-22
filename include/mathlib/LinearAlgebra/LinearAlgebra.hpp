6aa78e5c569ba7d9cee7597ef4799cc79ff625c8
saltyjoss <joss.salton@gmail.com>
2025-10-22 17:21:05 +0100

    feat(Matrix): Add 2x2 matrix header

--- include/mathlib/LinearAlgebra/LinearAlgebra.hpp
+++ include/mathlib/LinearAlgebra/LinearAlgebra.hpp
@@ -2,4 +2,5 @@
 
 #include "mathlib/LinearAlgebra/Vec2d.hpp"
 #include "mathlib/LinearAlgebra/Vec3d.hpp"
+#include "mathlib/LinearAlgebra/Matrix2x2.hpp"
 #include "mathlib/LinearAlgebra/Matrix3x3.hpp"
