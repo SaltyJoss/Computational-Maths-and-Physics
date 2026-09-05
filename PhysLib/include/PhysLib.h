800d66c7c465d7425911683c3b3810afc8a11575
saltyjoss <joss.salton@gmail.com>
2026-09-05 10:49:19 +0100

    feat: Update include guards and add collision header files

--- PhysLib/include/PhysLib.h
+++ PhysLib/include/PhysLib.h
@@ -1,7 +1,7 @@
 // PxM/PhysLib PhysLib.h
 
 // Definition of library (in case of multiple inclusion)
-#ifdef PHYS_LIB_H
+#ifndef PHYS_LIB_H
 #define PHYS_LIB_H
 
 #include <core/MathLib.h>
---
+++ PhysLib/include/collision/collision.h
@@ -0,0 +1,14 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "collision/OBB.h"
+#include "collision/capsule.h"
+#include "collision/convex_hull.h"
+#include "collision/convex_hull_build.h"
+#include "collision/contact.h"
+#include "collision/contact_solver.h"
+#include "collision/GJK.h"
+#include "collision/EPA.h"
+#include "collision/SAT.h"
+
+#endif // COLLISION_H
